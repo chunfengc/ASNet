d21b1ac89604eb9fe97a19a7637d0293320ba4e3
chunfengc <43671384+chunfengc@users.noreply.github.com>
2019-10-09 22:57:33 -0700

    Update FineTuning.py

--- ASNet/FineTuning.py
+++ ASNet/FineTuning.py
@@ -141,33 +141,4 @@ def train_kd(student, teacher, device, train_loader, optimizer, train_max_batch,
     return accuracy
 
 
-    
-# # ## Talget's code
-# def loss_fn_kd(outputs, labels, teacher_outputs, params):
-#     """
-#     Compute the knowledge-distillation (KD) loss given outputs, labels.
-#     "Hyperparameters": temperature and alpha
-#     NOTE: the KL Divergence for PyTorch comparing the softmaxs of teacher
-#     and student expects the input tensor to be log probabilities! See Issue #2
-#     """
-#     alpha = params['alpha']
-#     T = params['temperature']  
-#     KD_loss = nn.KLDivLoss()(F.log_softmax(outputs / T, dim=1),
-#                              F.softmax(teacher_outputs / T, dim=1))*(alpha*T*T) + \
-#               F.cross_entropy(outputs, labels) * (1. - alpha)
-
-#     return KD_loss
-
-# ## Talget's code
-
-# ## Talget's code
-# def display_loss(loss, xlabel='#iteration', ylabel='loss', title='Training loss'):
-#     display.clear_output(wait=True)
-#     plt.figure(figsize=(8, 6))
-#     plt.title(title)
-#     plt.xlabel(xlabel)
-#     plt.ylabel(ylabel)
-#     plt.plot(loss, 'g')
-#     plt.yscale("log")
-#     plt.show()
-
+ 
